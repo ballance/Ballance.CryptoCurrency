2a07bc43469a3e15ff88d5d6b75192adf10527cb
ballance <ballance@gmail.com>
2014-06-29 15:48:27 -0400

    initial commit

---
+++ CryptoConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bitThing = new Ballance.Crypto.Bitcoin();
+            var currentPriceRetrieved = bitThing.GetCurrentPrice();
+
+            //Console.WriteLine("Current Price: {0}", currentPriceRetrieved);
+
+            //var livePrice = bitThing.GetLivePrice("https://bitcoinaverage.com/#USD", "global-last");
+            
+            //Console.WriteLine("Current Live Price: {0}", livePrice);
+            
+            ////Console.Write("Press any key to continue. 'q' to quit.");
+            var entered = string.Empty;
+
+            while (!entered.Equals("x", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var livePrice = bitThing.GetLivePriceJSON("https://api.bitcoinaverage.com/ticker/USD/", "global-last");
+
+                Console.WriteLine("{0}", livePrice);
+
+                entered = Console.ReadKey().Key.ToString();
+            }
+            //Console.ReadKey();
+        }
+    }
+}
