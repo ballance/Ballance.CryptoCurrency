ef7ab07df6d6fd1610e62500fc59d022d15b50bb
ballance <ballance@gmail.com>
2014-06-29 15:42:18 -0400

    Initial version.  Console app calls Ballance.Crypto assembly to get latest Bitcoin pricein USD from bitcoinaverage.com

---
+++ Ballance.Crypto/Bitcoin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+
+namespace Ballance.Crypto
+{
+    public class Bitcoin
+    {
+        public string GetCurrentPrice()
+        {
+            return "550.991";
+        }
+
+        public string GetLivePrice(string url, string matcher)
+        {
+            var htmlWeb = new HtmlWeb();
+            var htmlDoc = htmlWeb.Load(url);
+
+            var price = htmlDoc.GetElementbyId(matcher).InnerText;
+
+            return price;
+        }
+
+        public string GetLivePriceJSON(string url, string p2)
+        {
+            using (var webClient = new System.Net.WebClient())
+            {
+                var json = string.Empty;
+                try
+                {
+                    json = webClient.DownloadString(url);
+
+                }
+                catch (WebException wex)
+                {
+                    if (wex.Message.Contains("The remote server returned an error: (429)"))
+                    {
+                        return "Error (429) You clicky too fast!";
+                    }
+                    return wex.ToString();
+                }
+                catch (Exception ex)
+                {
+                    return ex.ToString();
+                }
+                
+                dynamic JsonDe = JsonConvert.DeserializeObject(json);
+                return JsonDe["last"];
+            }
+        }
+    }
+}
